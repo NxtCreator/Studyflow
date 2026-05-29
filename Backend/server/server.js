5faba6323db187a0f297ed44b07e72a3fdf835e4
Gregory <serwaddagregory3@gmail.com>
2026-05-29 21:28:54 +0300

    Prepare backend for production deployment

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -5,6 +5,8 @@ import connectDB from "./config/db.js";
 import taskRoutes from "./routes/taskRoutes.js"
 import authRoutes from "./routes/authRoutes.js"
 
+const PORT = process.env.PORT || 3000;
+
 dotenv.config();
 
 connectDB()
@@ -22,6 +24,6 @@ app.get("/", (req, res) => {
     res.send("API is running...")
 });
 
-app.listen(3000, "0.0.0.0", () => {
-    console.log("Server is listening on port 3000")
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
 })
